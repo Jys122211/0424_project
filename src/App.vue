d9eb2348c53aa2ecdead75516cd40068a18045d1
Jys122211 <tns1592@naver.com>
2026-04-24 16:17:04 +0900

    Jys1 (#1)
    * Initial commit
    
    * first_commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  hello
+  <div class="card-container">
+    <Dongju />
+    <Hyoneung />
+    <Hyosun />
+    <Yunsang />
+  </div>
+</template>
+
+<script setup>
+import Dongju from '@/components/dongju.vue';
+import Hyoneung from '@/components/hyoneung.vue';
+import Hyosun from '@/components/hyosun.vue';
+import Yunsang from '@/components/yunsang.vue';
+</script>
+
+<style scoped>
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 200px);
+  gap: 20px;
+  padding: 20px;
+}
+</style>
